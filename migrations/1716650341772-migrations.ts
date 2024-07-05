a236665f86fd1070b9e7eccaa9c2e0ae096599d3
Braynner Silva <braynnert31@gmal.com>
2024-07-05 10:41:55 -0500

    modified migration add personalInformation

--- migrations/1716650341772-migrations.ts
+++ migrations/1716650341772-migrations.ts
@@ -12,7 +12,22 @@ export class Migrations1716650341772 implements MigrationInterface {
       `CREATE TABLE "file" ("fileId" uuid NOT NULL DEFAULT uuid_generate_v4(), "fileName" character varying(50) NOT NULL, "fileType" character varying NOT NULL DEFAULT true, "fileUrl" character varying NOT NULL, "fileLength" character varying(50) NOT NULL, CONSTRAINT "PK_f620cbf511fcf9b5970d187fdca" PRIMARY KEY ("fileId"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "user" ("userId" uuid NOT NULL DEFAULT uuid_generate_v4(), "userFullName" character varying(32) NOT NULL, "userLastName" character varying(32) NOT NULL, "username" character varying(24) NOT NULL, "userEmail" character varying(32) NOT NULL, "userPassword" character varying(151) NOT NULL, "userState" boolean NOT NULL DEFAULT true, "roleRoleId" uuid, "fileFileId" uuid, CONSTRAINT "REL_e9ac21f63535580f0c50b22817" UNIQUE ("fileFileId"), CONSTRAINT "PK_d72ea127f30e21753c9e229891e" PRIMARY KEY ("userId"))`,
+      `CREATE TABLE "product" ("productId" uuid NOT NULL DEFAULT uuid_generate_v4(), "productName" character varying(255) NOT NULL, "productDescription" text, "productCost" numeric(10,2) NOT NULL, "productUnitValue" numeric(10,2) NOT NULL, "productWholesaleValue" numeric(10,2) NOT NULL, "stock" integer NOT NULL, "minWholesaleQuantity" integer NOT NULL, CONSTRAINT "PK_429540a50a9f1fbf87efd047f35" PRIMARY KEY ("productId"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "sale_detail" ("saleDetailId" uuid NOT NULL DEFAULT uuid_generate_v4(), "quantity" integer NOT NULL, "unitPrice" numeric(10,2) NOT NULL, "subtotal" numeric(10,2) NOT NULL, "saleSaleId" uuid, "productProductId" uuid, CONSTRAINT "PK_d508e7c5bb31f30b0bbbca121d5" PRIMARY KEY ("saleDetailId"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "client" ("clientId" uuid NOT NULL DEFAULT uuid_generate_v4(), "clientIdentificacion" character varying(255) NOT NULL, "clientName" character varying(255) NOT NULL, "clientLastName" character varying(255) NOT NULL, "clientEmail" character varying(255) NOT NULL, "clientPhoneNumber" character varying(20) NOT NULL, CONSTRAINT "PK_6ed9067942d7537ce359e172ff6" PRIMARY KEY ("clientId"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "sale" ("saleId" uuid NOT NULL DEFAULT uuid_generate_v4(), "saleDate" TIMESTAMP NOT NULL DEFAULT now(), "total" numeric(10,2), "personalInformationId" uuid, "clientId" uuid, CONSTRAINT "PK_61e9fc39f22df5682850ea649f2" PRIMARY KEY ("saleId"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "personal_information" ("personalInformationId" uuid NOT NULL DEFAULT uuid_generate_v4(), "personalInformationLastName" character varying(32) NOT NULL, "personalInformationFullName" character varying(255) NOT NULL, "personalInformationEmail" character varying(32) NOT NULL, "personalInformationCellNumber" character varying(255) NOT NULL, "personalInformationAddres" character varying(32) NOT NULL, "personalInformationidentification" character varying(32) NOT NULL, CONSTRAINT "PK_d27ae7d71f575273c8c637804f8" PRIMARY KEY ("personalInformationId"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "user" ("userId" uuid NOT NULL DEFAULT uuid_generate_v4(), "userPassword" character varying(151) NOT NULL, "userState" boolean NOT NULL DEFAULT true, "username" character varying(255) NOT NULL, "roleRoleId" uuid NOT NULL, "fileFileId" uuid, "personalInformationId" uuid, CONSTRAINT "REL_e9ac21f63535580f0c50b22817" UNIQUE ("fileFileId"), CONSTRAINT "REL_3f04cee14c74af225aeb27f722" UNIQUE ("personalInformationId"), CONSTRAINT "PK_d72ea127f30e21753c9e229891e" PRIMARY KEY ("userId"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "roles_has_permissions" ("roleRoleId" uuid NOT NULL, "permissionPermissionId" uuid NOT NULL, CONSTRAINT "PK_af15ccac31be8675c639966fd4c" PRIMARY KEY ("roleRoleId", "permissionPermissionId"))`,
@@ -24,10 +39,25 @@ export class Migrations1716650341772 implements MigrationInterface {
       `CREATE INDEX "IDX_7a49bb3774e053242dee39cc5e" ON "roles_has_permissions" ("permissionPermissionId") `,
     );
     await queryRunner.query(
+      `ALTER TABLE "sale_detail" ADD CONSTRAINT "FK_5577177eb9ed86f21c841e04b0c" FOREIGN KEY ("saleSaleId") REFERENCES "sale"("saleId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale_detail" ADD CONSTRAINT "FK_c58161fdb7d682ef0e3fdd275ce" FOREIGN KEY ("productProductId") REFERENCES "product"("productId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale" ADD CONSTRAINT "FK_7aeed9d9c66d6fda0c7e5f3ecec" FOREIGN KEY ("personalInformationId") REFERENCES "personal_information"("personalInformationId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale" ADD CONSTRAINT "FK_1f170accf5236a71106a84ed97b" FOREIGN KEY ("clientId") REFERENCES "client"("clientId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
       `ALTER TABLE "user" ADD CONSTRAINT "FK_ffe3092db843bd8f90fcfe97da7" FOREIGN KEY ("roleRoleId") REFERENCES "role"("roleId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "user" ADD CONSTRAINT "FK_e9ac21f63535580f0c50b228179" FOREIGN KEY ("fileFileId") REFERENCES "file"("fileId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "FK_3f04cee14c74af225aeb27f722a" FOREIGN KEY ("personalInformationId") REFERENCES "personal_information"("personalInformationId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "roles_has_permissions" ADD CONSTRAINT "FK_3beb30d0cc8790859f16ad9ff96" FOREIGN KEY ("roleRoleId") REFERENCES "role"("roleId") ON DELETE CASCADE ON UPDATE CASCADE`,
@@ -35,16 +65,41 @@ export class Migrations1716650341772 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "roles_has_permissions" ADD CONSTRAINT "FK_7a49bb3774e053242dee39cc5e1" FOREIGN KEY ("permissionPermissionId") REFERENCES "permission"("permissionId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
-    await queryRunner.query(
-      `CREATE VIEW "user_view" AS SELECT "u"."userId" AS "userId", "u"."userFullName" AS "userFullName", "u"."userLastName" AS "userLastName", "u"."username" AS "username", "u"."userEmail" AS "userEmail", "u"."userState" AS "userState", "r"."roleName" AS "roleName", "f"."fileUrl" AS "fileUrl" FROM "user" "u" INNER JOIN "role" "r" ON "r"."roleId"="u"."roleRoleId"  LEFT JOIN "file" "f" ON "f"."fileId"="u"."fileFileId" GROUP BY "u"."userId", "u"."userFullName", "u"."userLastName", "u"."userState", "u"."userEmail", "u"."username" , "r"."roleName", "f"."fileUrl"`,
-    );
+    await queryRunner.query(`CREATE VIEW "user_view" AS 
+    SELECT 
+      "u"."userId" AS "userId", 
+      "pi"."personalInformationFullName" AS "userFullName", 
+      "pi"."personalInformationLastName" AS "userLastName", 
+      "pi"."personalInformationEmail" AS "userEmail", 
+      "u"."username" AS "username", 
+      "u"."userState" AS "userState", 
+      "r"."roleName" AS "roleName", 
+      "f"."fileUrl" AS "fileUrl"
+    FROM 
+      "user" "u"
+    INNER JOIN 
+      "personal_information" "pi" ON "u"."personalInformationId" = "pi"."personalInformationId"
+    INNER JOIN 
+      "role" "r" ON "u"."roleRoleId" = "r"."roleId"
+    LEFT JOIN 
+      "file" "f" ON "u"."fileFileId" = "f"."fileId"
+    GROUP BY 
+      "u"."userId", 
+      "pi"."personalInformationFullName", 
+      "pi"."personalInformationLastName", 
+      "pi"."personalInformationEmail", 
+      "u"."username", 
+      "u"."userState", 
+      "r"."roleName", 
+      "f"."fileUrl"
+  `);
     await queryRunner.query(
       `INSERT INTO "typeorm_metadata"("database", "schema", "table", "type", "name", "value") VALUES (DEFAULT, $1, DEFAULT, $2, $3, $4)`,
       [
         'public',
         'VIEW',
         'user_view',
-        'SELECT "u"."userId" AS "userId", "u"."userFullName" AS "userFullName", "u"."userLastName" AS "userLastName", "u"."username" AS "username", "u"."userEmail" AS "userEmail", "u"."userState" AS "userState", "r"."roleName" AS "roleName", "f"."fileUrl" AS "fileUrl" FROM "user" "u" INNER JOIN "role" "r" ON "r"."roleId"="u"."roleRoleId"  INNER JOIN "file" "f" ON "f"."fileId"="u"."fileFileId" GROUP BY "u"."userId", "u"."userFullName", "u"."userLastName", "u"."userState", "u"."userEmail", "u"."username" , "r"."roleName", "f"."fileUrl"',
+        'SELECT \n      "u"."userId" AS "userId", \n      "pi"."personalInformationFullName" AS "userFullName", \n      "pi"."personalInformationLastName" AS "userLastName", \n      "pi"."personalInformationEmail" AS "userEmail", \n      "u"."username" AS "username", \n      "u"."userState" AS "userState", \n      "r"."roleName" AS "roleName", \n      "f"."fileUrl" AS "fileUrl"\n    FROM \n      "user" "u"\n    INNER JOIN \n      "personal_information" "pi" ON "u"."personalInformationId" = "pi"."personalInformationId"\n    INNER JOIN \n      "role" "r" ON "u"."roleRoleId" = "r"."roleId"\n    LEFT JOIN \n      "file" "f" ON "u"."fileFileId" = "f"."fileId"\n    GROUP BY \n      "u"."userId", \n      "pi"."personalInformationFullName", \n      "pi"."personalInformationLastName", \n      "pi"."personalInformationEmail", \n      "u"."username", \n      "u"."userState", \n      "r"."roleName", \n      "f"."fileUrl"',
       ],
     );
   }
@@ -62,10 +117,25 @@ export class Migrations1716650341772 implements MigrationInterface {
       `ALTER TABLE "roles_has_permissions" DROP CONSTRAINT "FK_3beb30d0cc8790859f16ad9ff96"`,
     );
     await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "FK_3f04cee14c74af225aeb27f722a"`,
+    );
+    await queryRunner.query(
       `ALTER TABLE "user" DROP CONSTRAINT "FK_e9ac21f63535580f0c50b228179"`,
     );
     await queryRunner.query(
       `ALTER TABLE "user" DROP CONSTRAINT "FK_ffe3092db843bd8f90fcfe97da7"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale" DROP CONSTRAINT "FK_1f170accf5236a71106a84ed97b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale" DROP CONSTRAINT "FK_7aeed9d9c66d6fda0c7e5f3ecec"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale_detail" DROP CONSTRAINT "FK_c58161fdb7d682ef0e3fdd275ce"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sale_detail" DROP CONSTRAINT "FK_5577177eb9ed86f21c841e04b0c"`,
     );
     await queryRunner.query(
       `DROP INDEX "public"."IDX_7a49bb3774e053242dee39cc5e"`,
@@ -75,6 +145,11 @@ export class Migrations1716650341772 implements MigrationInterface {
     );
     await queryRunner.query(`DROP TABLE "roles_has_permissions"`);
     await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TABLE "personal_information"`);
+    await queryRunner.query(`DROP TABLE "sale"`);
+    await queryRunner.query(`DROP TABLE "client"`);
+    await queryRunner.query(`DROP TABLE "sale_detail"`);
+    await queryRunner.query(`DROP TABLE "product"`);
     await queryRunner.query(`DROP TABLE "file"`);
     await queryRunner.query(`DROP TABLE "role"`);
     await queryRunner.query(`DROP TABLE "permission"`);
--- migrations/1719333286138-migrations.ts
+++ migrations/1719333286138-migrations.ts
@@ -2,30 +2,50 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migrations1719333286138 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // Insertar el rol
     await queryRunner.query(`
-            INSERT INTO public."role" ("roleId","roleName","roleDescription","roleState") VALUES
-            ('4bb8811c-e550-4032-8e1d-4d0296ab60b6','admin','admin',true);
-          `);
+      INSERT INTO public."role" ("roleId", "roleName", "roleDescription", "roleState")
+      VALUES ('4bb8811c-e550-4032-8e1d-4d0296ab60b6', 'admin', 'admin', true);
+    `);
 
+    // Insertar la relación entre rol y permiso
     await queryRunner.query(`
-          INSERT INTO roles_has_permissions ("roleRoleId", "permissionPermissionId")
-          VALUES('4bb8811c-e550-4032-8e1d-4d0296ab60b6', '4df9e7e6-1d0f-4583-95ae-4850bbb4eccd');
-        `);
+      INSERT INTO roles_has_permissions ("roleRoleId", "permissionPermissionId")
+      VALUES ('4bb8811c-e550-4032-8e1d-4d0296ab60b6', '4df9e7e6-1d0f-4583-95ae-4850bbb4eccd');
+    `);
+
+    // Insertar en la tabla personal_information
     await queryRunner.query(`
-        INSERT INTO public.user ("userId","userFullName","userLastName","username","userEmail","userPassword","userState","roleRoleId", "fileFileId")
-        VALUES ('9db7a487-c944-47a2-a5f6-77a4287edcdf','admin','admin','admin','admin@admin.com','$2a$10$BbW6jeQa5DRe5u.uN8KMIeY2C82quaNRgD6on6YETvU0oz0mctNBO',true,'4bb8811c-e550-4032-8e1d-4d0296ab60b6', null);
-        `);
+      INSERT INTO public."personal_information" ("personalInformationId", "personalInformationFullName", "personalInformationLastName", "personalInformationEmail"  , "personalInformationCellNumber", "personalInformationAddres", "personalInformationidentification")
+      VALUES ('f9a4dc3e-af6b-4ef6-b45f-903bc9efcfe9', 'admin', 'admin', 'admin@admin.com', '3013810009', 'Cra 53 # 70-415', '1044588956');
+    `);
+
+    // Insertar en la tabla user, haciendo referencia a personal_information y al rol
+    await queryRunner.query(`
+      INSERT INTO public."user" ("userId", "userPassword", "userState", "username", "roleRoleId", "personalInformationId")
+      VALUES ('9db7a487-c944-47a2-a5f6-77a4287edcdf', '$2a$10$BbW6jeQa5DRe5u.uN8KMIeY2C82quaNRgD6on6YETvU0oz0mctNBO', true, 'admin', '4bb8811c-e550-4032-8e1d-4d0296ab60b6', 'f9a4dc3e-af6b-4ef6-b45f-903bc9efcfe9');
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    // Eliminar datos de la tabla roles_has_permissions
     await queryRunner.query(`
-            TRUNCATE TABLE roles_has_permissions RESTART IDENTITY CASCADE;
-          `);
+      TRUNCATE TABLE roles_has_permissions RESTART IDENTITY CASCADE;
+    `);
+
+    // Eliminar datos de la tabla role
     await queryRunner.query(`
-            TRUNCATE TABLE public."role" RESTART IDENTITY CASCADE;
-          `);
+      TRUNCATE TABLE public."role" RESTART IDENTITY CASCADE;
+    `);
+
+    // Eliminar datos de la tabla personal_information
     await queryRunner.query(`
-            TRUNCATE TABLE public.users RESTART IDENTITY CASCADE;
-          `);
+      TRUNCATE TABLE public."personal_information" RESTART IDENTITY CASCADE;
+    `);
+
+    // Eliminar datos de la tabla user
+    await queryRunner.query(`
+      TRUNCATE TABLE public."user" RESTART IDENTITY CASCADE;
+    `);
   }
 }
