d61589ca1967f68076f12de7a0e0fce8de1ea1b2
Braynner Silva <braynnert31@gmal.com>
2024-07-05 10:53:16 -0500

    deleted file of salesman

--- src/api/salesman/dto/update-salesman.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSalesmanDto } from './create-salesman.dto';
-
-export class UpdateSalesmanDto extends PartialType(CreateSalesmanDto) {}
