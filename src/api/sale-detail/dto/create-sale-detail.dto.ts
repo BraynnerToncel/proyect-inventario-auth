09a466f50fadc1c5ffb1e2a2d2c7a0b97440acad
Braynner Silva <braynnert31@gmail.com>
2024-07-10 14:16:51 -0500

    feat: update sale-detail resource

--- src/api/sale-detail/dto/create-sale-detail.dto.ts
+++ src/api/sale-detail/dto/create-sale-detail.dto.ts
@@ -1,11 +1,32 @@
-import { IsUUID, IsInt, Min, IsArray } from 'class-validator';
+import { ETypeOfPayment } from '@constant/type-of-payment/type-of-payment.constant';
+import { Type } from 'class-transformer';
+import {
+  IsUUID,
+  IsInt,
+  Min,
+  IsArray,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 
 export class CreateSaleDetailDto {
   @IsUUID()
   clientId: string;
 
   @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ProductQuantityDto)
   products: Array<ProductQuantityDto>;
+
+  @IsNumber()
+  saleTypeOfPayment: ETypeOfPayment;
+
+  @IsNumber()
+  saleMoneyReceived: number;
+
+  @IsString()
+  personalInformationId: string;
 }
 
 export class ProductQuantityDto {
