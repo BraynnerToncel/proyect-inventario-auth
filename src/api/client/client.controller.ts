09ca62418828fb927aa00f66ff194113898cb561
Braynner Silva <braynnert31@gmail.com>
2024-07-10 14:10:21 -0500

    refactor: replace public routes decorator with permission decorator

--- src/api/client/client.controller.ts
+++ src/api/client/client.controller.ts
@@ -10,40 +10,42 @@ import {
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
-import { ApiTags } from '@nestjs/swagger';
-import { Public } from '@decorator/routes-public.decorator';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { PermissionRequired } from '@decorator/permission.decorator';
+import { ValidPermission } from '@constant/permissions/permissions.constant';
 
 @ApiTags('Client')
+@ApiSecurity('x-token')
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
 
   @Post()
-  @Public()
+  @PermissionRequired(ValidPermission.settings_users_create)
   create(@Body() createClientDto: CreateClientDto) {
     return this.clientService.create(createClientDto);
   }
 
-  @Public()
   @Get()
+  @PermissionRequired(ValidPermission.settings_users_create)
   findAll() {
     return this.clientService.findAll();
   }
 
-  @Public()
   @Get(':id')
+  @PermissionRequired(ValidPermission.settings_users_create)
   findOne(@Param('id') id: string) {
     return this.clientService.findOne(id);
   }
 
-  @Public()
   @Put(':id')
+  @PermissionRequired(ValidPermission.settings_users_create)
   update(@Param('id') id: string, @Body() updateClientDto: UpdateClientDto) {
     return this.clientService.update(id, updateClientDto);
   }
 
-  @Public()
   @Delete(':id')
+  @PermissionRequired(ValidPermission.settings_users_create)
   remove(@Param('id') id: string) {
     return this.clientService.remove(id);
   }
